bdf3700119e7dd779f36d561daccd013467782fa
Heiko Pälike <hpaelike@marum.de>
2016-10-27 15:31:50 +0200

    ...

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,5 +1,13 @@
 import XCTest
-@testable import SilicaTests
+@testable import Silica
+
+#if os(OSX) || os(iOS) || os(watchOS)
+	func XCTMain(_ testCases: [XCTestCaseEntry]) { fatalError("Not Implemented. Linux only") }
+	
+	func testCase<T: XCTestCase>(_ allTests: [(String, (T) -> () throws -> Void)]) -> XCTestCaseEntry { fatalError("Not Implemented. Linux only") }
+	
+	struct XCTestCaseEntry { }
+#endif
 
 XCTMain([
      testCase(SilicaTests.allTests),
